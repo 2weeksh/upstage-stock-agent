cb2fdf68f32c55f3c36694eaa802b9431530e414
hj_git <alclssha4665@gmail.com>
2026-01-16 01:04:32 +0900

    fix: User DB email 삭제

--- app/models/user.py
+++ app/models/user.py
@@ -10,9 +10,6 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
 
-    # [수정] nullable=True 추가 (이메일 없어도 저장 가능하게)
-    email = Column(String, unique=True, index=True, nullable=True)
-
     hashed_password = Column(String)
     nickname = Column(String)
     created_at = Column(DateTime, default=datetime.now)
